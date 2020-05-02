f6acfdfafb6ce01923ac57517d2ffb66b3976f8f
Tarald Gåsbakk <tarald@ursolutions.no>
2020-05-02 14:48:39 +0200

    Remove unused import

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -3,7 +3,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu';
-import { Typography, Button } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 
 
