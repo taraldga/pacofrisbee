1614e7ddae6c29a781cbf26a68f4fbb95aa4551a
Tarald Gåsbakk <tarald@ursolutions.no>
2020-05-02 22:27:08 +0200

    Created a game overview

---
+++ src/modules/GameOverview/GameOverview.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import { fetchGames } from 'data/FrisbeegolfData';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import SendIcon from '@material-ui/icons/Send';
+import format from 'date-fns/format';
+import Divider from '@material-ui/core/Divider';
+import { Link } from 'react-router-dom';
+import { Game } from 'types/Game';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.background.paper,
+      margin: "15px"
+    },
+  }),
+);
+
+
+const GameOverview = () => {
+  let classes = useStyles()
+  let games = fetchGames();
+  return (
+    <div>
+      <List className={classes.root}>
+        {
+          games.sort((a,b) => b.date.getTime() - a.date.getTime()).map((game: Game, idx: number) => {
+            return (
+              <>
+                <Link to={`/game/${game.id}/1`}>
+                <ListItem>
+                  <ListItemText primary={game.field.name} secondary={format(game.date, "dd.MM.yyyy")} />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <SendIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+                </Link>
+                {idx < games.length-1 && <Divider />}
+              </>
+        )})}
+      </List>
+    </div>
+  )
+}
+
+
+export default GameOverview;
