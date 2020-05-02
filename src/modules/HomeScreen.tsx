7162763a7b2057835eaa5b075776caeebba54b0f
Tarald Gåsbakk <tarald@ursolutions.no>
2020-05-02 22:25:37 +0200

    Created a better homescreen view

--- src/modules/HomeScreen.tsx
+++ src/modules/HomeScreen.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 import Button from '@material-ui/core/Button';
+import SendIcon from '@material-ui/icons/Send';
+
 import { Link } from 'react-router-dom'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    layout: {
+      display: "flex",
+      flexDirection: "column",
+      textAlign: "center"
+    },
+  }),
+);
+
 const HomeScreen : React.FC = () => {
+    const classes = useStyles()
     return (
-        <div>
+        <div className={classes.layout}>
+            <h1>Welcom to PacoFrisbee</h1>
             <Link to="/create-game">
-                <Button variant="contained" color="primary" size="large">Start new round</Button>
+                <Button fullWidth variant="contained" color="primary" size="large" endIcon={<SendIcon />}>Start new round</Button>
             </Link>
-            <Link to="/view-games">
-                <Button variant="contained" color="primary" size="large">View my games</Button>
+            <Link to="/gameoverview">
+                <Button fullWidth variant="contained" color="primary" size="large">View my games</Button>
             </Link>
         </div>
     );
