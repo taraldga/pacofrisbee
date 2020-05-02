6ad2c6ddef7aa62ea4bc69782b74e01b6c4893d6
Tarald Gåsbakk <tarald@ursolutions.no>
2020-05-02 12:54:40 +0200

    Updated holeview to use tables instead

--- src/components/HoleView.tsx
+++ src/components/HoleView.tsx
@@ -3,6 +3,9 @@ import * as React from 'react'
 import PlayerRow from './PlayerRow'
 import { ScoreEntry } from 'types/ScoreEntry'
 import Player from 'types/Player'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableBody from '@material-ui/core/TableBody'
+import Table from '@material-ui/core/Table'
 
 export interface HoleViewProps {
   players: Player[];
@@ -20,14 +23,23 @@ const HoleView: React.FC<HoleViewProps> = ({
   return(
     <div>
       <h3>Hole number {holeNumber} </h3>
-      {
-        players.map(player => {
-          console.log(`player.id: ${player.id}`)
-          const playerScoreEntry = scoreEntries.find(entry => entry.playerId === player.id)
-          console.log(playerScoreEntry)
-          return <PlayerRow name={player.name} score={playerScoreEntry ? playerScoreEntry.score : 0}  updateScoreEntry={(newScore) => updateScoreEntry(player.id, newScore) }/>
-        })
-      }
+      <TableContainer>
+        <Table
+            aria-labelledby="tableTitle"
+            size={false ? 'small' : 'medium'}
+            aria-label="enhanced table"
+            >
+            <TableBody>
+            {
+              players.map(player => {
+                const playerScoreEntry = scoreEntries.find(entry => entry.playerId === player.id)
+                return <PlayerRow name={player.name} score={playerScoreEntry ? playerScoreEntry.score : 0}  updateScoreEntry={(newScore) => updateScoreEntry(player.id, newScore) }/>
+              })
+            }
+            </TableBody>
+        </Table>
+      </TableContainer>
+
     </div>
   )
 }
