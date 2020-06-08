29185679979547bb3fa0affa1a11a7e53f32c145
Tarald Gåsbakk <tarald@ursolutions.no>
2020-06-08 06:06:33 +0200

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,81 @@
-import './App.css';
+import "./App.css";
 
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { auth } from "firebase/app";
 
-import GameController from './modules/GameController/GameController';
-import GameCreator from './modules/GameCreator/GameCreator'
-import HomeScreen from 'modules/HomeScreen';
-import TopBar from 'components/TopBar/TopBar'
-import React from 'react';
-import GameOverview from 'modules/GameOverview/GameOverview';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import GameController from "./modules/GameController/GameController";
+import GameCreator from "./modules/GameCreator/GameCreator";
+import HomeScreen from "modules/HomeScreen";
+import TopBar from "components/TopBar/TopBar";
+import React from "react";
+import GameOverview from "modules/GameOverview/GameOverview";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export enum SignInState {
   waiting,
-  notSignedIn
+  notSignedIn,
 }
 
-
 function App() {
-  const [user, setUser] = React.useState<firebase.User | SignInState>(SignInState.waiting)
+  const [user, setUser] = React.useState<firebase.User | SignInState>(
+    SignInState.waiting
+  );
   // Configure FirebaseUI.
   const uiConfig = {
     // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
+    signInFlow: "popup",
     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: '/',
+    signInSuccessUrl: "/",
     // We will display Google and Facebook as auth providers.
-    signInOptions: [
-      auth.GoogleAuthProvider.PROVIDER_ID,
-    ]
+    signInOptions: [auth.GoogleAuthProvider.PROVIDER_ID],
   };
-  auth().onAuthStateChanged(function(user) {
+  auth().onAuthStateChanged(function (user) {
     if (user) {
-      setUser(user)
+      setUser(user);
     } else {
-      setUser(SignInState.notSignedIn)
+      setUser(SignInState.notSignedIn);
     }
   });
 
   let loginScreenJsx;
-  if(user === SignInState.notSignedIn){
-    loginScreenJsx = <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth()}/>
-  } else if(user === SignInState.waiting) {
-    loginScreenJsx = <CircularProgress />
+  if (user === SignInState.notSignedIn) {
+    loginScreenJsx = (
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth()} />
+    );
+  } else if (user === SignInState.waiting) {
+    loginScreenJsx = <CircularProgress />;
   }
 
   return (
-      <>
-        <Router>
-          <TopBar />
-          {
-            (user !== SignInState.waiting && user !== SignInState.notSignedIn) ? (
-                    <div className="App">
-                      <Switch>
-                        <Route exact path="/">
-                          <HomeScreen />
-                        </Route>
-                        <Route exact path="/create-game">
-                          <GameCreator />
-                        </Route>
-                        <Route exact path="/game/:gameId/:holeId" component={GameController}>
-                        </Route>
-                        <Route exact path="/gameoverview">
-                          <GameOverview />
-                        </Route>
-                      </Switch>
-                    </div>
-                ):
-                (
-                    loginScreenJsx
-                )
-          }
-        </Router>
-      </>
-  )
+    <>
+      <Router>
+        <TopBar />
+        {user !== SignInState.waiting && user !== SignInState.notSignedIn ? (
+          <div className="App">
+            <Switch>
+              <Route exact path="/">
+                <HomeScreen />
+              </Route>
+              <Route exact path="/create-game">
+                <GameCreator />
+              </Route>
+              <Route
+                exact
+                path="/game/:gameId/:holeId"
+                component={GameController}
+              />
+              <Route exact path="/gameoverview">
+                <GameOverview />
+              </Route>
+            </Switch>
+          </div>
+        ) : (
+          loginScreenJsx
+        )}
+      </Router>
+    </>
+  );
 }
 
 export default App;
