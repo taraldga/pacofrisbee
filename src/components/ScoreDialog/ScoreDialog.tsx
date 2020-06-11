d9ef645c641825e382d05380c193d8aacbb8c575
taraldga <taraldga@gmail.com>
2020-06-11 21:34:03 +0200

    fix: renamed score button

--- src/components/ScoreDialog/ScoreDialog.tsx
+++ src/components/ScoreDialog/ScoreDialog.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
-import Dialog from "@material-ui/core/Dialog";
+
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+
 import Button from "@material-ui/core/Button";
-import { TransitionProps } from "@material-ui/core/transitions/transition";
+import { CenteredLoader } from "components/CenteredLoader/CenteredLoader";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Player from "types/Player";
+import { ScoreEntry } from "types/ScoreEntry";
 import Slide from "@material-ui/core/Slide";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -9,13 +17,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import { CenteredLoader } from "components/CenteredLoader/CenteredLoader";
-import { ScoreEntry } from "types/ScoreEntry";
-import Player from "types/Player";
+import { TransitionProps } from "@material-ui/core/transitions/transition";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -73,7 +75,7 @@ const ScoreDialog: React.FC<ScoreDialogProps> = ({
       TransitionComponent={Transition}
       className={classes.dialog}
     >
-      <DialogTitle>Current standings</DialogTitle>
+      <DialogTitle>Score</DialogTitle>
       <DialogContent>
         {scoreEntries.length > 0 ? (
           <TableContainer>
