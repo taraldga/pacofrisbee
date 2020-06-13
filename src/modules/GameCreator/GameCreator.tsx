809202db20ae744af568989afa27b7c3cd48cd47
Tarald Gåsbakk <tarald@ursolutions.no>
2020-06-13 17:21:51 +0200

    Updated color scheme and styling on gamecreator

--- src/modules/GameCreator/GameCreator.tsx
+++ src/modules/GameCreator/GameCreator.tsx
@@ -1,184 +1,273 @@
-import "./GameCreator.css"
+import "./GameCreator.css";
 
-import { Checkbox, MenuItem, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
-import { createGame, getFields, getPlayers, savePlayer } from 'data/FrisbeegolfData';
+import React, { useEffect, useState } from "react";
+import {
+  createGame,
+  getFields,
+  getPlayers,
+  savePlayer,
+} from "data/FrisbeegolfData";
 
-import Button from '@material-ui/core/Button';
-import Field from 'types/Field';
-import FormControl from '@material-ui/core/FormControl';
-import { GameData } from 'types/Game';
-import InputLabel from '@material-ui/core/InputLabel';
-import Player from 'types/Player';
-import Select from '@material-ui/core/Select';
-import TextField from '@material-ui/core/TextField';
-import uniqid from 'uniqid'
+import Button from "@material-ui/core/Button";
+import Field from "types/Field";
+import FormControl from "@material-ui/core/FormControl";
+import { GameData } from "types/Game";
+import InputLabel from "@material-ui/core/InputLabel";
+import Player from "types/Player";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
+import uniqid from "uniqid";
 import { useHistory } from "react-router-dom";
 import Loader from "components/Loader/Loader";
+import Paper from "@material-ui/core/Paper/Paper";
+import MenuItem from "@material-ui/core/MenuItem/MenuItem";
+import TableContainer from "@material-ui/core/TableContainer/TableContainer";
+import Table from "@material-ui/core/Table/Table";
+import TableHead from "@material-ui/core/TableHead/TableHead";
+import TableRow from "@material-ui/core/TableRow/TableRow";
+import TableCell from "@material-ui/core/TableCell/TableCell";
+import TableBody from "@material-ui/core/TableBody/TableBody";
+import Checkbox from "@material-ui/core/Checkbox/Checkbox";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import createStyles from "@material-ui/core/styles/createStyles";
 
-const GameCreator : React.FC = () => {
-    let history = useHistory();
-    const [fields, setFields] = useState<Field[]>([])
 
-    const [game, setGame] = useState<GameData>({
-        id: uniqid(),
-        date: new Date(),
-        field: {name: "", holes: []},
-        players: [],
-        scoreEntries: [] 
-    })
-
-    const [newPlayerName, setNewPlayerName] = useState("")
-    const [error, setError] = useState<{[key: string]: string}>({})
-    const [players, setPlayers] = useState<Player[]>([])
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    
-    useEffect(() => {
-        const fetchData = async () => {
-          let fields = await getFields()
-          setFields(fields)
-          let players = await getPlayers()
-          setPlayers(players);
-          setIsLoading(false);
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    gameCreator: {
+        textAlign: "center",
+        height: "100%",
+        backgroundColor: ""
+    },
+    addPlayerRow: {
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        margin: "20px 0",
+    },
+    addplayerInput: {
+        width: "200px"
+    },
+    inputGroup: {
+        margin: "20px 10px",
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center",
+        "& h3": {
+            backgroundColor: theme.palette.primary.main,
+            margin: "0",
+            padding: "10px 0",
+            textAlign: "center",
+            color: "white",
         }
-        fetchData()
-      }, [])
-
-    const updateField = (event: React.ChangeEvent<{ value: unknown }>) => {
-        const newField = fields.find(field => field.id === event.target.value);
-        if(newField) {
-            setGame({
-                ...game,
-                field: newField
-            })
-        }
+    },
+    inputTable: {
+        maxHeight: "400px",
+        width: "98%",
+        margin: "10px auto"
+    },
+    inputDropDown: {
+        width: "98%",
+        margin: "10px auto"
+    },
+    ".Mui-selected": {
+        backgroundColor: theme.palette.primary.main,
+        
     }
+  })
+);
 
-    const updatePlayers = (newPlayer: Player) => {
-        let newPlayers = Array.from(game.players);
-        const playerIndex = newPlayers.findIndex(player => player.id === newPlayer.id)
+const GameCreator: React.FC = () => {
+  let history = useHistory();
+  const [fields, setFields] = useState<Field[]>([]);
 
-        if(playerIndex === -1) {
-            newPlayers.push(newPlayer)
-        } else {
-            newPlayers.splice(playerIndex, 1);
-        }
-        setGame({
-            ...game,
-            players: newPlayers
-        })
+  const [game, setGame] = useState<GameData>({
+    id: uniqid(),
+    date: new Date(),
+    field: { name: "", holes: [] },
+    players: [],
+    scoreEntries: [],
+  });
+
+  const [newPlayerName, setNewPlayerName] = useState("");
+  const [error, setError] = useState<{ [key: string]: string }>({});
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let fields = await getFields();
+      setFields(fields);
+      let players = await getPlayers();
+      setPlayers(players);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const updateField = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const newField = fields.find((field) => field.id === event.target.value);
+    if (newField) {
+      setGame({
+        ...game,
+        field: newField,
+      });
     }
+  };
 
-    const startGame = async () => {
-        setIsLoading(true);
-        let error = verifyInput(game);
-        if(Object.keys(error).length === 0 && error.constructor === Object) {
-            game.playerList = game.players.map(player => player.name);
-            let dbGame = await createGame(game)
-            history.push(`/game/${dbGame.id}/${1}`)
-        } else {
-            setError(error)
-        }
-    }
-
-    const addPlayer = () => {
-        const newPlayer: Player = {
-            id: uniqid(),
-            name: newPlayerName
-        }
-        players.push(newPlayer)
-        savePlayer(newPlayer);
-        updatePlayers(newPlayer)
-        setNewPlayerName("")
-    }
-    
-    const isSelected = (playerId: string) => game.players.findIndex(player => player.id === playerId) > -1
-
-    const verifyInput = (game: GameData) => {
-        let error: {[key: string]: string} = {};
-
-        if( game.field.name === "" ) {
-            error["field"] = "You must select a field"
-        }
-        if( game.players.length === 0 ) {
-            error["players"] = "You must select at least one player"
-        }
-        return error
-    }
-    
-    return (
-        <div className="game-creator-view">
-        <Loader isOpen={isLoading} text="Loading..." wait={700} />
-        <h3>Select field</h3>
-        <div className="input-group">
-            <FormControl className="gamecreator-select" error={ error["field"]!== undefined}>
-                <InputLabel>Select Field</InputLabel>
-                <Select
-                    placeholder="Select field"
-                    value={game?.field.id}
-                    onChange={updateField}
-                >
-                    {fields.map((field) => (
-                        <MenuItem key={field.id} value={field.id}>{field.name}</MenuItem>
-                    ))}
-                </Select>
-
-            </FormControl>
-        </div>
-        <h3>Select players</h3>
-        <div className="input-group">
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell align="right">Checked</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-
-                    {players.map((player, index) => {
-                        const isItemSelected = isSelected(player.id);
-                        const labelId = `enhanced-table-checkbox-${index}`;
-
-                        return (
-                            <TableRow
-                                hover
-                                onClick={(_) => updatePlayers(player)}
-                                role="checkbox"
-                                aria-checked={isItemSelected}
-                                tabIndex={-1}
-                                key={player.id}
-                                selected={isItemSelected}
-                            >
-                            <TableCell align="left">{player.name}</TableCell>
-                            <TableCell align="right" padding="checkbox">
-                                <Checkbox
-                                    checked={isItemSelected}
-                                    inputProps={{ 'aria-labelledby': labelId }}
-                                />
-                            </TableCell>
-                            </TableRow>
-                        );
-                        })}
-                    <TableRow>
-                        <TableCell >
-                            <TextField onChange={(e) => setNewPlayerName(e.target.value)} value={newPlayerName} label="Player Name" variant="standard" />
-                        </TableCell>
-                        <TableCell align="right">
-                            <Button color="default" size="small" variant="contained" onClick={() => addPlayer()} >Add</Button>
-                        </TableCell>
-                    </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-        <div className="input-group">
-            <Button fullWidth color="primary" size="large" variant="contained" onClick={startGame}>Start Game</Button>
-        </div>
-        </div>
+  const updatePlayers = (newPlayer: Player) => {
+    let newPlayers = Array.from(game.players);
+    const playerIndex = newPlayers.findIndex(
+      (player) => player.id === newPlayer.id
     );
-}
 
+    if (playerIndex === -1) {
+      newPlayers.push(newPlayer);
+    } else {
+      newPlayers.splice(playerIndex, 1);
+    }
+    setGame({
+      ...game,
+      players: newPlayers,
+    });
+  };
 
-export default GameCreator
+  const startGame = async () => {
+    setIsLoading(true);
+    let error = verifyInput(game);
+    if (Object.keys(error).length === 0 && error.constructor === Object) {
+      game.playerList = game.players.map((player) => player.name);
+      let dbGame = await createGame(game);
+      history.push(`/game/${dbGame.id}/${1}`);
+    } else {
+      setError(error);
+    }
+  };
+
+  const addPlayer = () => {
+    const newPlayer: Player = {
+      id: uniqid(),
+      name: newPlayerName,
+    };
+    players.push(newPlayer);
+    savePlayer(newPlayer);
+    updatePlayers(newPlayer);
+    setNewPlayerName("");
+  };
+
+  const isSelected = (playerId: string) =>
+    game.players.findIndex((player) => player.id === playerId) > -1;
+
+  const verifyInput = (game: GameData) => {
+    let error: { [key: string]: string } = {};
+
+    if (game.field.name === "") {
+      error["field"] = "You must select a field";
+    }
+    if (game.players.length === 0) {
+      error["players"] = "You must select at least one player";
+    }
+    return error;
+  };
+
+  return (
+    <div className={classes.inputGroup}>
+      <Loader isOpen={isLoading} text="Loading..." wait={700} />
+      <Paper className={classes.inputGroup}>
+        <h3>Select field</h3>
+        <FormControl
+          error={error["field"] !== undefined}
+          className={classes.inputDropDown}
+        >
+          <InputLabel>Select Field</InputLabel>
+          <Select
+            placeholder="Select field"
+            value={game?.field.id}
+            onChange={updateField}
+          >
+            {fields.map((field) => (
+              <MenuItem key={field.id} value={field.id}>
+                {field.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Paper>
+      <Paper className={classes.inputGroup}>
+        <h3>Select players</h3>
+        <TableContainer className={classes.inputTable}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Checked</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {players.map((player, index) => {
+                const isItemSelected = isSelected(player.id);
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    hover
+                    onClick={(_) => updatePlayers(player)}
+                    role="checkbox"
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={player.id}
+                    selected={isItemSelected}
+                  >
+                    <TableCell align="left">{player.name}</TableCell>
+                    <TableCell align="right" padding="checkbox">
+                      <Checkbox
+                        checked={isItemSelected}
+                        inputProps={{ "aria-labelledby": labelId }}
+                      />
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+              <TableRow>
+                <TableCell>
+                  <TextField
+                    onChange={(e) => setNewPlayerName(e.target.value)}
+                    value={newPlayerName}
+                    label="Player Name"
+                    variant="standard"
+                  />
+                </TableCell>
+                <TableCell align="right">
+                  <Button
+                    color="default"
+                    size="small"
+                    variant="contained"
+                    onClick={() => addPlayer()}
+                  >
+                    Add
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+      <div className={classes.inputGroup}>
+        <Button
+          fullWidth
+          color="primary"
+          size="large"
+          variant="contained"
+          onClick={startGame}
+        >
+          Start Game
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default GameCreator;
