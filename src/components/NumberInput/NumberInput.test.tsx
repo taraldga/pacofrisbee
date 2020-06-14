248faee8504559472486c61b8c1e9ec95d6f24ae
taraldga <taraldga@gmail.com>
2020-06-14 09:19:46 +0200

    Updated tests

--- src/components/NumberInput/NumberInput.test.tsx
+++ src/components/NumberInput/NumberInput.test.tsx
@@ -1,7 +1,6 @@
+import NumberInput from './NumberInput';
 import React from 'react';
 import { render } from '@testing-library/react';
-import NumberInput from './NumberInput';
-
 
 test('renders one input', () => {
   const { container } = render(<NumberInput value={3} isDirty={true} onChange={() => {}} />);
@@ -27,12 +26,4 @@ test('renders the given value', () => {
   const linkElement = container.getElementsByTagName("input");
 
   expect(linkElement[0].value).toBe("3")
-})
-
-test('is black when no change is done', () => {
-  const { container } = render(<NumberInput value={3} isDirty={false} onChange={() => {}} />);
-  
-  const linkElement = container.getElementsByTagName("input");
-
-  expect(linkElement[0].style.color).toBe("black")
 })
--- src/util/findScoreForPlayer.test.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-
-describe('calculateScoreForPlayer', () => {
-  
-})
