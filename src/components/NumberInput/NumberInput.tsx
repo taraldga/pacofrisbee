69e9722c973667e2baef70ab330f66ca128e6364
Tarald Gåsbakk <tarald@ursolutions.no>
2020-08-29 20:41:23 +0200

    Improved numberinput design

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -2,10 +2,9 @@ import './NumberInput.css'
 
 import * as React from 'react'
 
-import Add from '@material-ui/icons/Add'
 import Button from '@material-ui/core/Button'
+import Add from '@material-ui/icons/Add'
 import Remove from '@material-ui/icons/Remove'
-import TextField from '@material-ui/core/TextField'
 
 export interface NumberInputProps {
   value: number;
@@ -38,7 +37,7 @@ const NumberInput : React.FC<NumberInputProps> = ({
   return (
     <div className="button-group">
       <Button variant="contained" color="primary" onClick={() => {onInnerChange(value-1)}}><Remove /></Button>
-      <TextField disabled className={`number-input ${isDirty && "dirty-score"}`} variant="outlined" value={value} onChange={(e) => onInnerChange(e.target.value.length > 0 ? parseInt(e.target.value): 0)} />
+      <p className={`number-input ${isDirty && "dirty-score"}`}>{ value }</p>
       <Button variant="contained" color="primary" onClick={() => onInnerChange(value+1)}><Add /></Button>
     </div>
   )
